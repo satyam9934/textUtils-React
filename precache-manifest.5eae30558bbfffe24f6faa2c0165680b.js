e43583c14fd8ed3fc558baa89de558d913bf2f04
satyamlernohub <satyam@lernohub.com>
2021-11-25 14:25:52 +0530

    Updates

---
+++ precache-manifest.5eae30558bbfffe24f6faa2c0165680b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4070adb7c2505781f27c04b78605b6c6",
+    "url": "/textUtils-React/index.html"
+  },
+  {
+    "revision": "4dd07755128c8e8024f3",
+    "url": "/textUtils-React/static/css/main.5060da72.chunk.css"
+  },
+  {
+    "revision": "5da4d59910867ec6fb96",
+    "url": "/textUtils-React/static/js/2.cd8ee3dc.chunk.js"
+  },
+  {
+    "revision": "fb6fca4f0fa26a7e27d26480a74532c9",
+    "url": "/textUtils-React/static/js/2.cd8ee3dc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7ee441d0356dac962f9e",
+    "url": "/textUtils-React/static/js/3.bb56fe81.chunk.js"
+  },
+  {
+    "revision": "4dd07755128c8e8024f3",
+    "url": "/textUtils-React/static/js/main.e8836b90.chunk.js"
+  },
+  {
+    "revision": "6d5568aa45895b8feb2c",
+    "url": "/textUtils-React/static/js/runtime-main.8d8592b4.js"
+  }
+]);
